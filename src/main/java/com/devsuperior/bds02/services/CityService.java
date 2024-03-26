62033e125a48c2847ffa6de87cb076cd5269f339
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-03-26 13:01:45 -0300

    City insert ok

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -22,9 +22,18 @@ public class CityService {
 	}
 	
 	
-	@Transactional
+	@Transactional(readOnly = true)
 	public List<CityDTO> findAll() {
 		List<City> list = repository.findAll(Sort.by("name"));
 		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		City entity = new City();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CityDTO(entity);
+		
 	}
 }
